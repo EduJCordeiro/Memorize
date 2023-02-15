e94c7032aa9dfd8ee3f8ff68751dbe4c4cd86698
Eduardo José Cordeiro <edujcordeiro99@gmail.com>
2023-02-14 23:42:57 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eduardo José Cordeiro on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
